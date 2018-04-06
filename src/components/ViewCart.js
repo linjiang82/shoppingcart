22e0084b4fa7b30e8eb3a2c6e0fdde1c01afe51b
linjiang82 <linjiang82@gmail.com>
2018-04-06 20:01:33 +1000

    click the close button, now it can return to previous path in history.

--- src/components/ViewCart.js
+++ src/components/ViewCart.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './ViewCart.css';
 import {Link} from 'react-router-dom';
+import {Button} from 'react-bootstrap';
 
 let ViewCart = (props) =>{ 
     console.log(props) ;
     return(<div className='viewCart'>
         <div>
             {/*go backk to / ATM, will goback to history last entry*/}
-            <Link to='/' className='closeCart'>X</Link>
+            {/*<Link to='/' className='closeCart'>X</Link>*/}
+            <Button className='closeCart' onClick={()=>props.history.goBack()}>X</Button>
         </div>
         <ul className='cartDetail'>{props.cartDetail}</ul>
     </div>)
