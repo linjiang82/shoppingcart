5675d5ffadfd058249b7e33b267091a3affd1e2a
linjiang82 <linjiang82@gmail.com>
2019-01-05 19:19:04 +1000

    add search function

--- src/containers/ProWrap.js
+++ src/containers/ProWrap.js
@@ -1,20 +1,29 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import Products from '../components/Products';
+import Special from '../components/Special';
+import Popular from '../components/Popular';
 import {products} from '../reducers';
 import {addToCart} from '../actions/userActions';
 import {withRouter} from 'react-router';
 
-const mapStateToProps = (state) => (
-    {
-        items:state.products,
+const mapStateToProps = (state) =>{ 
+    console.log(state.products);
+    let items = state.products.filter(i =>
+        i.name.toLowerCase().includes(state.proList.searchKeyword.toLowerCase())
+    )
+    return {
+        items:items,
     }
-)
+}
+
 
 const mapDispatchToProps = (dispatch) => {
     return {
         onClick: (item) => dispatch(addToCart(item)),
     }
 }
-
-export default withRouter(connect(mapStateToProps,mapDispatchToProps)(Products));
+let myConnect = connect(mapStateToProps,mapDispatchToProps);
+export const Pro =  withRouter(myConnect(Products));
+export const Sp = withRouter(myConnect(Special));
+export const Pop = withRouter(myConnect(Popular)); 
