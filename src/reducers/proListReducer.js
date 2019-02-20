ecbf991696816ef755bbaeba4369ddb1fc07974b
linjiang82 <linjiang82@gmail.com>
2019-02-20 10:45:06 +1000

    add reducers of select and deselect categories

--- src/reducers/proListReducer.js
+++ src/reducers/proListReducer.js
@@ -1,8 +1,11 @@
+import {removeProperty} from '../utils/utils'
+
 const initState = {
   loading:false,
   prolist:[],
   error:null,
-  searchKeyword:''
+  searchKeyword:'',
+  categoryKeyword:{}
 }
 
 const proListReducer = (state=initState,action) =>{
@@ -33,6 +36,40 @@ const proListReducer = (state=initState,action) =>{
               searchKeyword:action.keyword
           }
       break;
+      case 'selectCat':
+      let keyword=action.keyword.split(':');
+      if(keyword[0] in state.categoryKeyword) {  
+        if(!state.categoryKeyword[keyword[0]].includes(keyword[1]))
+            {
+            return state = {
+                    ...state,
+                    categoryKeyword:{...state.categoryKeyword,
+                    [keyword[0]]:[...state.categoryKeyword[keyword[0]],keyword[1]]}
+            }
+          }
+        else return state;
+        }
+      else {
+        return state = {
+                ...state,
+                categoryKeyword:{...state.categoryKeyword,
+                [keyword[0]]:[keyword[1]]}
+        }
+      }
+      break;
+      case 'delSelectCat':
+      {
+      let keyword = action.keyword.split(' ');
+      let restKeyword = state.categoryKeyword[keyword[0]].filter(i=>i!=keyword[1])
+      restKeyword = restKeyword.length==0?removeProperty(state.categoryKeyword, keyword[0]):{...state.categoryKeyword,[keyword[0]]:restKeyword}
+      console.log(restKeyword)
+      
+      return state = {
+              ...state,
+              categoryKeyword: restKeyword
+              }
+      break;
+        }
   
     default:
       return state;
