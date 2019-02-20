0a267aaa9ca21288dcf3aa71aa925df8ac5f130b
linjiang82 <linjiang82@gmail.com>
2019-02-20 10:40:29 +1000

    major change to the layout of page

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Pro} from './containers/ProWrap';
+import {Pro, Sp, Pop} from './containers/ProWrap';
 import Head from './components/Head';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/store';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Banner from './components/Banner';
+import Prolist from './components/ProList'
+import './index.css'
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-        <div>
-                <Route path='/' component={Head} />
+        <div className='ctner'>
+                <Route path='/' component={Head} className='head'/>
+                <Banner className='banner'/>
+                <Prolist className='prolist'/>
                 <Route exact path='/' component={Pro} />
+                <Route exact path='/Home' component={Pro} />
+                <Route exact path='/Special' component={Sp} />
+                <Route exact path='/Popular' component={Pop} />
         </div>
         </Router>
     </Provider>, document.getElementById('root'));
